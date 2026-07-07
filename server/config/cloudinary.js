38db229f3f245e9c5369180ace02a86a0a9ac16c
monikajangid7860 <monikajangid93143@gmail.com>
2026-07-07 23:03:00 +0530

    Fix CORS for Vercel deployments

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,5 +1,11 @@
 const cloudinary = require("cloudinary").v2;
 
+console.log("========== CLOUDINARY ==========");
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("Secret Exists:", !!process.env.CLOUDINARY_API_SECRET);
+console.log("================================");
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
