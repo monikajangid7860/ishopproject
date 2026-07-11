1f3260d1ac9f0c574269fa47c3586b743cc69393
monikajangid7860 <monikajangid93143@gmail.com>
2026-07-12 00:38:07 +0530

    Fix CORS for Vercel deployments

--- client/src/components/website/Header.jsx
+++ client/src/components/website/Header.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { ChevronRight } from "lucide-react";
+import { ChevronRight,LogIn } from "lucide-react";
 import Link from "next/link";
 import {
   Menu,
